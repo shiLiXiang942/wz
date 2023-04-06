c2c62ba06517d8d9b80e57b682ca883b2bfba2df
shiLiXiang942 <slx9755@163.com>
2023-04-06 10:33:27 +0800

    提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Welcome:
+    <a href="#" target="_blank">HBuilderX</a>
+  </p>
+
+  <p>
+    <a href="#" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="#" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
